4180920b3ba4371c84ef4fdaf2c11730f36174b2
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2023-06-24 22:56:39 +0200

    Make enum comments inline

--- include/uf2ota/uf2types.h
+++ include/uf2ota/uf2types.h
@@ -76,23 +76,23 @@ typedef struct {
 } uf2_info_t;
 
 typedef enum {
-	UF2_TAG_VERSION	  = 0x9FC7BC, // version of firmware file - UTF8 semver string
-	UF2_TAG_PAGE_SIZE = 0x0BE9F7, // page size of target device (32 bit unsigned number)
-	UF2_TAG_SHA2	  = 0xB46DB0, // SHA-2 checksum of firmware (can be of various size)
-	UF2_TAG_DEVICE	  = 0x650D9D, // description of device (UTF8)
-	UF2_TAG_DEVICE_ID = 0xC8A729, // device type identifier
+	UF2_TAG_VERSION	  = 0x9FC7BC, //!< version of firmware file - UTF8 semver string
+	UF2_TAG_PAGE_SIZE = 0x0BE9F7, //!< page size of target device (32 bit unsigned number)
+	UF2_TAG_SHA2	  = 0xB46DB0, //!< SHA-2 checksum of firmware (can be of various size)
+	UF2_TAG_DEVICE	  = 0x650D9D, //!< description of device (UTF8)
+	UF2_TAG_DEVICE_ID = 0xC8A729, //!< device type identifier
 	// format versions
 	UF2_TAG_OTA_FORMAT_1 = 0x5D57D0,
 	UF2_TAG_OTA_FORMAT_2 = 0x6C8492,
 	// LibreTiny custom tags
-	UF2_TAG_OTA_PART_LIST = 0x6EC68A, // list of OTA schemes this package is usable in
-	UF2_TAG_OTA_PART_INFO = 0xC0EE0C, // partition names for each target type
-	UF2_TAG_BOARD		  = 0xCA25C8, // board name (lowercase code)
-	UF2_TAG_FIRMWARE	  = 0x00DE43, // firmware description / name
-	UF2_TAG_BUILD_DATE	  = 0x822F30, // build date/time as Unix timestamp
-	UF2_TAG_BINPATCH	  = 0xB948DE, // binary patch to convert OTA1->OTA2
-	UF2_TAG_FAL_PTABLE	  = 0x8288ED, // FAL partition table length (stored in block padding)
-	UF2_TAG_LT_VERSION	  = 0x59563D, // LT version (semver)
+	UF2_TAG_OTA_PART_LIST = 0x6EC68A, //!< list of OTA schemes this package is usable in
+	UF2_TAG_OTA_PART_INFO = 0xC0EE0C, //!< partition names for each target type
+	UF2_TAG_BOARD		  = 0xCA25C8, //!< board name (lowercase code)
+	UF2_TAG_FIRMWARE	  = 0x00DE43, //!< firmware description / name
+	UF2_TAG_BUILD_DATE	  = 0x822F30, //!< build date/time as Unix timestamp
+	UF2_TAG_BINPATCH	  = 0xB948DE, //!< binary patch to convert OTA1->OTA2
+	UF2_TAG_FAL_PTABLE	  = 0x8288ED, //!< FAL partition table length (stored in block padding)
+	UF2_TAG_LT_VERSION	  = 0x59563D, //!< LT version (semver)
 } uf2_tag_type_t;
 
 typedef enum {
@@ -110,20 +110,20 @@ typedef enum {
 
 typedef enum {
 	UF2_ERR_OK			  = 0,
-	UF2_ERR_IGNORE		  = 1,	// block should be ignored
-	UF2_ERR_MAGIC		  = 2,	// wrong magic numbers
-	UF2_ERR_FAMILY		  = 3,	// family ID mismatched
-	UF2_ERR_NOT_HEADER	  = 4,	// block is not a header
-	UF2_ERR_OTA_VER		  = 5,	// unknown/invalid OTA format version
-	UF2_ERR_OTA_WRONG	  = 6,	// no data for current OTA scheme
-	UF2_ERR_PART_404	  = 7,	// no partition with that name
-	UF2_ERR_PART_INVALID  = 8,	// invalid partition info tag
-	UF2_ERR_PART_UNSET	  = 9,	// image broken - attempted to write without target partition
-	UF2_ERR_DATA_TOO_LONG = 10, // data too long - tags won't fit
-	UF2_ERR_SEQ_MISMATCH  = 11, // sequence number mismatched
-	UF2_ERR_ERASE_FAILED  = 12, // erasing flash failed
-	UF2_ERR_WRITE_FAILED  = 13, // writing to flash failed
-	UF2_ERR_WRITE_LENGTH  = 14, // wrote fewer data than requested
+	UF2_ERR_IGNORE		  = 1,	//!< block should be ignored
+	UF2_ERR_MAGIC		  = 2,	//!< wrong magic numbers
+	UF2_ERR_FAMILY		  = 3,	//!< family ID mismatched
+	UF2_ERR_NOT_HEADER	  = 4,	//!< block is not a header
+	UF2_ERR_OTA_VER		  = 5,	//!< unknown/invalid OTA format version
+	UF2_ERR_OTA_WRONG	  = 6,	//!< no data for current OTA scheme
+	UF2_ERR_PART_404	  = 7,	//!< no partition with that name
+	UF2_ERR_PART_INVALID  = 8,	//!< invalid partition info tag
+	UF2_ERR_PART_UNSET	  = 9,	//!< image broken - attempted to write without target partition
+	UF2_ERR_DATA_TOO_LONG = 10, //!< data too long - tags won't fit
+	UF2_ERR_SEQ_MISMATCH  = 11, //!< sequence number mismatched
+	UF2_ERR_ERASE_FAILED  = 12, //!< erasing flash failed
+	UF2_ERR_WRITE_FAILED  = 13, //!< writing to flash failed
+	UF2_ERR_WRITE_LENGTH  = 14, //!< wrote fewer data than requested
 } uf2_err_t;
 
 // compatibility macros
